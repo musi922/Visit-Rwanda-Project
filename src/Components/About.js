a0a54455349aef2338204bd573e7d8dd16f1d88e
unknown <richardmusime6@gmail.com>
2023-04-21 17:50:01 +0200

    first-commit

--- src/Components/About.js
+++ src/Components/About.js
@@ -68,7 +68,7 @@ const About = () => {
             </button>
 
       </div>
-       <div className='bodys'>
+       <div className='bodys' >
         <img src={hero} className='Photos' style={{width: isSmallScreen? "100%":""}}/>
         <div className='text'>
           <div className='logistics ' style={{display: isSmallScreen?"none":""}}>
@@ -90,42 +90,42 @@ const About = () => {
           <img src={Abouts} style={{width:"100%", height:"25vh", marginTop: isSmallScreen? "-400px":"",marginLeft: isSmallScreen? "-45px":"", width: isSmallScreen? "250px":""}} />
         </div>
       </div>
-      <div className='blog' style={{marginTop: isSmallScreen?"-124%":""}}>
-        <div className='blogs'>
-          <img src={miss} style={{height:"45vh", width:"100%", border:"1px solid transparent", borderRadius:"8px", marginTop: isSmallScreen?"118%":"", marginleft: isSmallScreen?"-2px":"", position: isSmallScreen?"absolute":"", width: isSmallScreen?"300px":""}}/>
+      <div className='blog' style={{marginTop: isSmallScreen?"-64%":"", height: isSmallScreen ? '300vh':""}}>
+        <div className='blogs' style={{width: isSmallScreen?"300px":"", marginTop: isSmallScreen?"60%":"", position: isSmallScreen?"absolute":""}}>
+          <img src={miss} style={{height:"45vh", width: "100%", border:"1px solid transparent", borderRadius:"8px", }} />
         </div>
         <div className='hup'>
           <h1 style={{marginTop: isSmallScreen?"-34%":"", marginLeft: isSmallScreen?"-36%":"", fontSize: isSmallScreen?"25px":"", position: isSmallScreen?"absolute":"",}}> Company mission</h1>
           <p style={{marginTop: isSmallScreen?"-44%":"", marginLeft: isSmallScreen?"-96%":"", fontSize: isSmallScreen?"14px":"",  width: isSmallScreen?"310px":""}}>“Success is based on pro-active and dedicated teamwork in a professional and ethical environment...” </p>
           <h2 style={{marginTop: isSmallScreen?"-74%":"", marginLeft: isSmallScreen?"-96%":"", fontSize: isSmallScreen?"12px":"",  width: isSmallScreen?"350px":""}}>We strongly believe that success is based on pro-active and dedicated teamwork in a professional and ethical environment that leads to creative solutions and fully deserved rewards. This said, we always focus on safety, quality, a detailed approach, and above all, diligent handling of budgetary means to maintain high standards at all levels of our activities, especially in dealings with employees, suppliers, subcontractors, and customers.</h2>
         </div>
-        <div className='vis'>
-        <h1> Company culture</h1>
-        <h2><GoPrimitiveDot/>  Foster a professional and ethical work environment.</h2>
-        <h2><GoPrimitiveDot/>  Set safety and quality as our top priorities.</h2>
-        <h2><GoPrimitiveDot/>  Encourage creativity and innovation in every aspect .</h2>
-        <h2><GoPrimitiveDot/>  Recognize the value of a continuous improvement.</h2>
-        <h2><GoPrimitiveDot/>  Be open, listen to our customers, and adapt to change.</h2>
+        <div className='vis' style={{marginTop: isSmallScreen?"20%":""}}>
+        <h1 style={{fontSize: isSmallScreen?"25px":"",}}> Company culture</h1>
+        <h2 style={{fontSize: isSmallScreen?"14px":"", width: isSmallScreen?"350px":"",  marginLeft: isSmallScreen?"-34px":"",}}><GoPrimitiveDot/>  Foster a professional and ethical work environment.</h2>
+        <h2 style={{fontSize: isSmallScreen?"14px":"", width: isSmallScreen?"350px":"",  marginLeft: isSmallScreen?"-34px":"",}}><GoPrimitiveDot/>  Set safety and quality as our top priorities.</h2>
+        <h2 style={{fontSize: isSmallScreen?"14px":"", width: isSmallScreen?"350px":"",  marginLeft: isSmallScreen?"-34px":"",}}><GoPrimitiveDot/>  Encourage creativity and innovation in every aspect .</h2>
+        <h2 style={{fontSize: isSmallScreen?"14px":"", width: isSmallScreen?"350px":"",  marginLeft: isSmallScreen?"-34px":"",}}><GoPrimitiveDot/>  Recognize the value of a continuous improvement.</h2>
+        <h2 style={{fontSize: isSmallScreen?"14px":"", width: isSmallScreen?"350px":"",  marginLeft: isSmallScreen?"-34px":"",}}><GoPrimitiveDot/>  Be open, listen to our customers, and adapt to change.</h2>
         </div>
-        <div className='fot'>
+        <div className='fot' style={{width: isSmallScreen?"300px":"", marginTop: isSmallScreen?"3%":"", display: isSmallScreen?"none":"", }}>
         <img src={culs} style={{height:"45vh", width:"100%", border:"1px solid transparent", borderRadius:"8px"}}/>
         </div>
-        <div className='hai'>
+        <div className='hai' style={{marginTop: isSmallScreen?"10%":""}}>
         <img src={hai} style={{height:"60vh", width:"100%", border:"1px solid transparent", borderRadius:"8px"}}/>
-        <h1>Company vision </h1>
-        <p>We aim to be a leading contractor in Rwanda by focusing on the timely implementation of our projects and meeting the expectations and requirements of our customers at the highest possible level.</p>
+        <h1 style={{fontSize: isSmallScreen?"25px":"", marginTop: isSmallScreen?"-74%":"",  marginLeft: isSmallScreen?"9%":"",}}>Company vision </h1>
+        <p style={{marginTop: isSmallScreen?"-64%":"", marginLeft: isSmallScreen?"6%":"", fontSize: isSmallScreen?"14px":"",  width: isSmallScreen?"310px":"" , }}>We aim to be a leading contractor in Rwanda by focusing on the timely implementation of our projects and meeting the expectations and requirements of our customers at the highest possible level.</p>
         </div>
         <div className='founders'>
-          <h1>Our Founders </h1>
+          <h1 style={{fontSize: isSmallScreen?"25px":"", marginTop: isSmallScreen?"4%":"",  marginLeft: isSmallScreen?"9%":"",}}>Our Founders </h1>
           <div className='fou'>
-        <img src={shema} style={{height:"45vh", width:"90%", border:"1px solid transparent", borderRadius:"8px"}}/>
-        <h2>Mr. Shema Frank</h2>
-        <h3>Mr. Shema Frank</h3>
-        <p>Bachelor’s degree in Applied Mathematics and Data Engineering from University of Rwanda, Cairo, Egypt, 2002. With 20 years of experience in the construction field for the main contractor of the below-listed construction projects before founding Projex-Tec.</p>
+        <img src={shema} style={{height:"45vh", width:"90%", border:"1px solid transparent", borderRadius:"8px" , width: isSmallScreen? "300px":"", marginTop: isSmallScreen?"80%":"", position: isSmallScreen?"absolute":""}}/>
+        <h2 style={{fontSize: isSmallScreen?"16px":"", width: isSmallScreen?"350px":"",  display: isSmallScreen?"none":"",}}>Mr. Shema Frank</h2>
+        <h3 style={{marginTop: isSmallScreen?"10%":"", marginLeft: isSmallScreen?"20%":"", fontSize: isSmallScreen?"15px":"",  width: isSmallScreen?"310px":"" , }}>Mr. Shema Frank</h3>
+        <p style={{marginTop: isSmallScreen?"15%":"", marginLeft: isSmallScreen?"6%":"", fontSize: isSmallScreen?"14px":"",  width: isSmallScreen?"310px":"" , }}>Bachelor’s degree in Applied Mathematics and Data Engineering from University of Rwanda, Cairo, Egypt, 2002. With 20 years of experience in the construction field for the main contractor of the below-listed construction projects before founding Projex-Tec.</p>
         </div>
         </div>
       </div>
-      <div className='ing'>
+      <div className='ing' style={{marginTop: isSmallScreen?"4%":""}}>
       <Footer/>
       </div>
     </div>
